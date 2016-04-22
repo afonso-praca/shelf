c01408c2e947b91e0c8db5d4948696d4fb201b5c
Anderson Moreira <anderson.moreira@vtex.com.br>
2016-04-22 01:29:43 -0300

    Fix rebase conflicts

--- src/components/Shelf/Shelf.js
+++ src/components/Shelf/Shelf.js
@@ -2,8 +2,8 @@ import { editable } from 'vtex-editor';
 import React from 'react';
 import './Shelf.less';
 import './ShelfCustom.less';
-import ShelfPlaceholder from './ShelfPlaceholder';
-import ShelfSlider from './ShelfSlider';
+import ShelfPlaceholder from './ShelfPlaceholder/ShelfPlaceholder';
+import ShelfSlider from './ShelfSlider/ShelfSlider';
 
 @editable({
   name: 'Shelf@pilateslovers.shelf',
